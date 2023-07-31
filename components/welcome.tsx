82d772ef985957b93d1bd677a6a56e2d65735b89
Alex Vongseng <alex.vongseng@gmail.com>
2023-07-31 10:17:49 -0500

    fixed unescaped error

--- components/welcome.tsx
+++ components/welcome.tsx
@@ -1,5 +1,6 @@
 
 export const Welcome = () => {
+    const about = "Im a passionate and dedicated student at the University of Minnesota, studying Information Technology Infrastructure and Computer Science. I have a strong interest in full stack development and I'm currently honing my skills in using modern tools like React, Next.js, Tailwind CSS, Langchain, and Supabase."
     return (
         <header className='h-[75vh] '>
             <div className='flex flex-col justify-center h-full space-y-6'>
@@ -15,7 +16,7 @@ export const Welcome = () => {
                 </div>
                 <div>
                     <p className='text-sm md:text-lg lg:max-w-[75%]'>
-                    I'm a passionate and dedicated student at the University of Minnesota, studying Information Technology Infrastructure and Computer Science. I have a strong interest in full stack development and I'm currently honing my skills in using modern tools like React, Next.js, Tailwind CSS, Langchain, and Supabase.
+                        {about}
                     </p>
                 </div>
                 <div>
