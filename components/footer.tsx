8874ca43f906fba5919fd197c702b9ab1b2eca3e
Alex Vongseng <alex.vongseng@gmail.com>
2023-08-01 11:51:30 -0500

    Added my avatar to footer

--- components/footer.tsx
+++ components/footer.tsx
@@ -1,12 +1,19 @@
+import { Avatar, AvatarFallback, AvatarImage } from '@/components/ui/avatar'
 import Link from 'next/link'
 
 export const Footer = () => {
     return (
         <footer className="border-t p-6 sticky">
-            <div className="flex justify-between max-w-4xl mx-auto">
-                <p className="text-semibold text-xs text-zinc-600">
-                    Made by <Link className="underline" href="/">Alex</Link>
-                </p>
+            <div className="flex justify-between max-w-4xl mx-auto items-center">
+                <div className='flex gap-2 items-center'>
+                    <Avatar className='aspect-square w-6 h-6'>
+                        <AvatarImage src="https://github.com/alex-von.png" alt="Alex Vongseng" />
+                        <AvatarFallback>AV</AvatarFallback>
+                    </Avatar>
+                    <p className="text-semibold text-xs text-zinc-600">
+                        Made by <Link className="underline" href="/">Alex</Link>
+                    </p>
+                </div>
                 <p className="text-xs text-zinc-600">
                     © 2023 Alex Vongseng
                 </p>
