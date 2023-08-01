1912c4d2ef416aa7e4a1b6753a7bdc06ea22ff51
Alex Vongseng <alex.vongseng@gmail.com>
2023-08-01 00:16:01 -0500

    Cleaned up code

--- components/projects.tsx
+++ components/projects.tsx
@@ -13,37 +13,6 @@ type Project = {
     timeframe: string
 }
 
-const projects = [
-    {
-      title: 'Notion ToDo App',
-      description: 'A simple todo app built with Next.js, Tailwind CSS, and Supabase.',
-      technologies: ['NextJS', 'Tailwind CSS', 'Supabase'],
-      url: '',
-      github_url: 'https://github.com/user/alex-von',
-      image_url: '',
-      timeframe: 'June 2023',
-    },
-    // Add more projects as needed...
-    {
-      title: 'shotby.alexv',
-      description: 'A photography portfolio for my photography to showcase my work and let clients book photoshoots and view their photo.',
-      technologies: ['NextJS', 'Tailwind CSS', 'React', 'TypeScript', 'Supabase', 'Stripe'],
-      url: 'https://shotbyalexv.com',
-      github_url: 'https://github.com/alex-von',
-      image_url: '',
-      timeframe: 'July 2023',
-    },
-    {
-      title: 'Portfolio Website',
-      description: 'A portfolio website to showcase my full-stack development skills and projects.',
-      technologies: ['NextJS', 'Tailwind CSS', 'React', 'TypeScript', 'Supabase'],
-      url: '',
-      github_url: 'https://github.com/alex-von', 
-      image_url: '',
-      timeframe: 'August 2023',
-    },
-  ];
-
 const ProjectCard = (project: Project) => {
     return (
         <div className="flex flex-col space-y-3 rounded-xl border p-6 cursor-pointer">
@@ -71,6 +40,38 @@ const ProjectCard = (project: Project) => {
 }
 
 export const Projects = () => {
+    // Place holder content
+    const projects = [
+        {
+          title: 'Notion ToDo App',
+          description: 'A simple todo app built with Next.js, Tailwind CSS, and Supabase.',
+          technologies: ['NextJS', 'Tailwind CSS', 'Supabase'],
+          url: '',
+          github_url: 'https://github.com/user/alex-von',
+          image_url: '',
+          timeframe: 'June 2023',
+        },
+        // Add more projects as needed...
+        {
+          title: 'shotby.alexv',
+          description: 'A photography portfolio for my photography to showcase my work and let clients book photoshoots and view their photo.',
+          technologies: ['NextJS', 'Tailwind CSS', 'React', 'TypeScript', 'Supabase', 'Stripe'],
+          url: 'https://shotbyalexv.com',
+          github_url: 'https://github.com/alex-von',
+          image_url: '',
+          timeframe: 'July 2023',
+        },
+        {
+          title: 'Portfolio Website',
+          description: 'A portfolio website to showcase my full-stack development skills and projects.',
+          technologies: ['NextJS', 'Tailwind CSS', 'React', 'TypeScript', 'Supabase'],
+          url: '',
+          github_url: 'https://github.com/alex-von', 
+          image_url: '',
+          timeframe: 'August 2023',
+        },
+    ];
+
     return (
         <div className="flex flex-col space-y-6 mx-auto p-4 w-full">
             <div>
