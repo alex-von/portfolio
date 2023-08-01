4beecbd1b3c354b44ab528bd8b7b90cf23e3331d
Alex Vongseng <alex.vongseng@gmail.com>
2023-08-01 10:14:29 -0500

    Added placeholders for posts and projects pages

--- app/posts/page.tsx
+++ app/posts/page.tsx
@@ -4,7 +4,10 @@ export default function Home() {
   return (
     <main className="w-full min-h-screen flex flex-col">
       <PostHeader />
-      <div className="md:flex mx-auto my-12 w-full max-w-4xl ">
+      <div className="md:flex mx-auto my-12 w-full max-w-4xl p-4">
+          <div className="flex flex-col space-y-3 rounded-xl bg-secondary p-6 animate-pulse w-full">
+              <h3 className="text-xs">Posts Coming Soon</h3>
+          </div>
       </div>
     </main>
   )
