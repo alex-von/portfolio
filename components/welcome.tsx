cecf55370dff216621844a651a2f55bfeb27252d
Alex Vongseng <alex.vongseng@gmail.com>
2023-08-01 10:00:42 -0500

    Removed about me for now

--- components/welcome.tsx
+++ components/welcome.tsx
@@ -12,10 +12,10 @@ export const Welcome = () => {
                         <p className="text-xs font-medium text-zinc-500">
                             CS & IT @ University of Minnesota - Twin Cities
                         </p>
-                        <a href="https://linkedin.com/in/alex-vongseng" className="hover:underline flex items-center text-sm font-medium mt-6">
+                        {/* <a href="https://linkedin.com/in/alex-vongseng" className="hover:underline flex items-center text-sm font-medium mt-6">
                             About me
                             <ArrowRightIcon className="w-4 h-4 ml-2" />
-                        </a>
+                        </a> */}
                     </div>
                 </div>
             </div>
