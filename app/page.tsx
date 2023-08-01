50e437b600e7289a9b1ceb447c5dc348e97457ea
Alex Vongseng <alex.vongseng@gmail.com>
2023-07-31 22:02:21 -0500

    Added projects to main page

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
 import { Header } from "@/components/header"
+import { Projects } from "@/components/projects"
 
 export default function Home() {
   return (
-    <main className="w-full min-h-screen flex flex-col justify-between">
+    <main className="w-full min-h-screen flex flex-col">
       <Header />
+      <Projects />
     </main>
   )
 }
