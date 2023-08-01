d6c7fa57a9066a6ccee017de4cfb9c57405163e4
Alex Vongseng <alex.vongseng@gmail.com>
2023-07-31 23:34:35 -0500

    Add placeholders for posts

--- components/post.tsx
+++ components/post.tsx
@@ -1,9 +1,51 @@
+
+type Post = {
+    title: string,
+    description: string,
+    url: string,
+    image_url: string,
+}
+
+// generate a list of post
+const posts: Post[] = [
+    {
+        title: 'Notion ToDo App', 
+        description: 'A simple todo app built with Next.js, Tailwind CSS, and Supabase.', 
+        url: '', 
+        image_url: ''
+    },
+    {
+        title: 'shotby.alexv', 
+        description: 'A photography portfolio for my photography to showcase my work and let clients book photoshoots and view their photo.', 
+        url: 'https://shotbyalexv.com', 
+        image_url: ''
+    },
+    {
+        title: 'Portfolio Website', 
+        description: 'A portfolio website to showcase my full-stack development skills and projects.', 
+        url: '', 
+        image_url: ''
+    }
+]
+
+const PostCard = (post: Post) => {
+    return (
+        <div className="flex flex-col space-y-3 rounded-xl border p-6 shadow cursor-pointer hover:bg-accent transition ease-in-out">
+            <h3 className="font-medium text-sm">{post.title}</h3>
+            <p className="text-xs">{post.description}</p>
+        </div>
+    )
+}
+
 export const Post = () => {
     return (
         <div className="flex flex-col mx-auto p-4 w-full">
             <h1 className="font-medium">Post</h1>
             <p className="text-xs text-zinc-500 mb-6">Things I wrote</p>
             <div className="space-y-4">
+                {posts.map((posts, index) => (
+                    <PostCard key={index} {...posts} />
+                ))}
             </div>
         </div>
     )
