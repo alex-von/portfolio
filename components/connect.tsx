608757becc35ec0f6db65ab68bdcd9a2112bd383
Alex Vongseng <alex.vongseng@gmail.com>
2023-08-02 09:31:17 -0500

    Updated connect component

--- components/connect.tsx
+++ components/connect.tsx
@@ -10,11 +10,11 @@ export const Connect = () => {
             <div className="space-y-4 rounded-xl border p-4">
                 <a className='flex gap-3 items-center text-sm font- underline' target="_blank" href="https://www.linkedin.com/in/alex-vongseng/">
                     <LinkedInLogoIcon className="h-[1.2rem] w-[1.2rem] text-zinc-700 dark:text-white"/>
-                    linkedin.com/in/alex-vongseng/
+                    /in/alex-vongseng/
                 </a>
                 <a className='flex gap-3 items-center text-sm  underline' target="_blank" href="https://github.com/alex-von">
                     <GitHubLogoIcon className="h-[1.2rem] w-[1.2rem] text-zinc-700 dark:text-white"/>
-                    github.com/alex-von
+                    /alex-von
                 </a>
                 <a className='flex gap-3 items-center text-sm font- underline' href="mailto:alex.vongseng@gmail.com">
                     <EnvelopeClosedIcon className="h-[1.2rem] w-[1.2rem] text-zinc-700 dark:text-white"/>
