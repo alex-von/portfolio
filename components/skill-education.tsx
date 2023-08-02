385a8fa686e765150f68e3581b6f29fc99c3a1d1
Alex Vongseng <alex.vongseng@gmail.com>
2023-08-02 00:53:21 -0500

    Added more info to cards

--- components/skill-education.tsx
+++ components/skill-education.tsx
@@ -1,7 +1,86 @@
-import { GitHubLogoIcon, LinkedInLogoIcon, EnvelopeClosedIcon, FileTextIcon } from '@radix-ui/react-icons'
-import { SiJavascript, SiNextdotjs, SiPython, SiSupabase, SiTailwindcss, SiTypescript } from 'react-icons/si'
+'use client'
+
+import { 
+    SiJavascript, 
+    SiNextdotjs, 
+    SiPython, 
+    SiTailwindcss, 
+    SiTypescript, 
+    SiNodedotjs ,
+    SiC,
+    SiGit,
+    SiMysql
+} from 'react-icons/si'
+
+import { FaJava } from 'react-icons/fa'
+import { Button } from '@/components/ui/button'
+import { useState } from 'react'
+import { ChevronDownIcon, ChevronUpIcon, ArrowRightIcon } from '@radix-ui/react-icons'
+import Link from 'next/link'
 
 export const Skills = () => {
+    const [showMore, setShowMore] = useState(false)
+
+    const moreSkills = 
+        <>
+            <li className='flex justify-between gap-2 items-center text-sm font-medium text-zinc-800 dark:text-white'>
+                <div className='flex items-center gap-2'>
+                    <SiNodedotjs className="w-4 h-4" />
+                    <p>Node.js</p>   
+                </div>
+                <div className='border-b w-full'></div>
+                <p className='text-zinc-500'>Basic</p>
+            </li>
+            <li className='flex justify-between gap-2 items-center text-sm font-medium text-zinc-800 dark:text-white'>
+                <div className='flex items-center gap-2'>
+                    <SiGit className="w-4 h-4" />
+                    <p>Git</p>   
+                </div>
+                <div className='border-b w-full'></div>
+                <p className='text-zinc-500'>Basic</p>
+            </li>
+            <li className='flex justify-between gap-2 items-center text-sm font-medium text-zinc-800 dark:text-white'>
+                <div className='flex items-center gap-2'>
+                    <SiTypescript className="w-4 h-4" />
+                    <p>Typescript</p>   
+                </div>
+                <div className='border-b w-full'></div>
+                <p className='text-zinc-500'>Basic</p>
+            </li>
+            <li className='flex justify-between gap-2 items-center text-sm font-medium text-zinc-800 dark:text-white'>
+                <div className='flex items-center gap-2'>
+                    <SiTailwindcss className="w-4 h-4" />
+                    <p>Tailwind</p>   
+                </div>
+                <div className='border-b w-full'></div>
+                <p className='text-zinc-500'>Basic</p>
+            </li>
+            <li className='flex justify-between gap-2 items-center text-sm font-medium text-zinc-800 dark:text-white'>
+                <div className='flex items-center gap-2'>
+                    <FaJava className="w-4 h-4" />
+                    <p>Java</p>   
+                </div>
+                <div className='border-b w-full'></div>
+                <p className='text-zinc-500'>Basic</p>
+            </li>
+            <li className='flex justify-between gap-2 items-center text-sm font-medium text-zinc-800 dark:text-white'>
+                <div className='flex items-center gap-2'>
+                    <SiC className="w-4 h-4" />
+                    <p>C</p>   
+                </div>
+                <div className='border-b w-full'></div>
+                <p className='text-zinc-500'>Basic</p>
+            </li>
+            <li className='flex justify-between gap-2 items-center text-sm font-medium text-zinc-800 dark:text-white'>
+                <div className='flex items-center gap-2'>
+                    <SiMysql className="w-4 h-4" />
+                    <p>MySQL</p>   
+                </div>
+                <div className='border-b w-full'></div>
+                <p className='text-zinc-500'>Basic</p>
+            </li>
+        </>
+
     return (
         <div className="flex flex-col space-y-4 mx-auto p-4 w-full">
             <div>
@@ -13,9 +92,16 @@ export const Skills = () => {
                     <h1 className='text-sm font-medium'>
                         Univeristy of Minnesota
                     </h1>
-                    <p className='text-xs text-zinc-500'>
-                        Bachelor of Science in Computer Science | May 2024
-                    </p>
+                    <div className='flex justify-between text-xs text-zinc-500'>
+                        <p>Bachelor of Applied Science</p>
+                        <p>May 2024</p>
+                    </div>
+                    <p className='text-xs mt-4'>Information Technology Infrastructure</p>
+                    <p className='text-xs'>Computer Science</p>
+                    <Link href='/projects' className="flex gap-2 justify-end text-xs font-medium text-zinc-500 hover:underline hover:text-white transition">
+                        Learn More
+                        <ArrowRightIcon className='w-4 h-4' />
+                    </Link>
                 </div>
             </div>
             <div className="space-y-4 rounded-xl border p-4">
@@ -23,36 +109,63 @@ export const Skills = () => {
                     <h1 className='text-sm font-medium'>
                         Anoka Rasmey Community College
                     </h1>
+                    <div className='flex justify-between text-xs text-zinc-500'>
+                        <p>Associate Degree</p>
+                        <p>Aug 2022</p>
+                    </div>
+                </div>
+            </div>
+            <ul className="rounded-xl border p-4 space-y-4">
+                <div>
+                    <h1 className='text-sm font-medium'>
+                        Techonologies and Languages
+                    </h1>
                     <p className='text-xs text-zinc-500'>
-                        General Education | August 2021
+                        Things I know and have used
                     </p>
                 </div>
-            </div>
-            <div className="grid grid-cols-2 space-y-4 rounded-xl border p-4">
-                <div className='flex gap-4 items-center text-sm font-medium text-zinc-800 dark:text-white'>
-                    <SiNextdotjs className="w-4 h-4" />
-                    Next.js
-                </div>
-                <div className='flex gap-4 items-center text-sm font-medium text-zinc-800 dark:text-white'>
-                    <SiTailwindcss className="w-4 h-4" />
-                    Tailwind CSS
-                </div>
-                <div className='flex gap-4 items-center text-sm font-medium text-zinc-800 dark:text-white'>
-                    <SiSupabase className="w-4 h-4" />
-                    Supabase
-                </div>
-                <div className='flex gap-4 items-center text-sm font-medium text-zinc-800 dark:text-white'>
-                    <SiPython className="w-4 h-4" />
-                    Python
-                </div>
-                <div className='flex gap-4 items-center text-sm font-medium text-zinc-800 dark:text-white'>
-                    <SiJavascript className="w-4 h-4" />
-                    Javascript
-                </div><div className='flex gap-4 items-center text-sm font-medium text-zinc-800 dark:text-white'>
-                    <SiTypescript className="w-4 h-4" />
-                    Typescript
-                </div>
-            </div>
+                <li className='flex justify-between gap-2 items-center text-sm font-medium text-zinc-800 dark:text-white'>
+                    <div className='flex items-center gap-2'>
+                        <SiJavascript className="w-4 h-4" />
+                        <p>Javascript</p>   
+                    </div>
+                    <div className='border-b w-full'></div>
+                    <p className='text-zinc-500'>Intermediate</p>
+                </li>
+                <li className='flex justify-between gap-2 items-center text-sm font-medium text-zinc-800 dark:text-white'>
+                    <div className='flex items-center gap-2'>
+                        <SiPython className="w-4 h-4" />
+                        <p>Python</p>   
+                    </div>
+                    <div className='border-b w-full'></div>
+                    <p className='text-zinc-500'>Intermediate</p>
+                </li>
+                <li className='flex justify-between gap-2 items-center text-sm font-medium text-zinc-800 dark:text-white'>
+                    <div className='flex items-center gap-2'>
+                        <SiNextdotjs className="w-4 h-4" />
+                        <p>Next.js</p>   
+                    </div>
+                    <div className='border-b w-full'></div>
+                    <p className='text-zinc-500'>Basic</p>
+                </li>
+
+                { showMore ? moreSkills : null}
+                
+                <Button variant='ghost' className='w-full rounded-xl text-xs text-zinc-500' asChild onClick={() => setShowMore(!showMore)}>
+                    { showMore ? 
+                        <div className='flex gap-2'>
+                            <p>Show Less</p>
+                            <ChevronUpIcon className='w-4 h-4' />
+                        </div> 
+                        : 
+                        <div className='flex gap-2' >
+                            <p>Show More</p>
+                            <ChevronDownIcon className='w-4 h-4' />
+                        </div>
+                    }
+                </Button>
+
+            </ul>
         </div>
     )
 }
