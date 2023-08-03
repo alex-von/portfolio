84d542812f9dbdb9b2772f93f8bb0f100ab8c429
Alex Vongseng <alex.vongseng@gmail.com>
2023-08-03 10:07:32 -0500

    Added Vercel Web Analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,7 @@ import { Inter } from 'next/font/google'
 import { ThemeProvider } from "@/components/theme-provider"
 import Head from 'next/head'
 import { Footer } from '@/components/footer'
+import { Analytics } from '@vercel/analytics/react'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -29,6 +30,7 @@ export default function RootLayout({
             <Footer />
           </div>
         </ThemeProvider>
+        <Analytics />
       </body>
     </html>
   )
